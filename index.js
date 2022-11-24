0952ee1b118ca33bf32b50ba6b81081bdd403b96
AkhdanFirdaus <akhdan.musyaffa.firdaus@gmail.com>
2022-11-24 11:07:46 +0700

    [ADD] use local auth

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hello guys')
+const qrcode = require('qrcode-terminal')
+
+const { Client, LocalAuth } = require('whatsapp-web.js')
+
+const client = new Client({
+  authStrategy: new LocalAuth({
+    clientId: 'client-1'
+  })
+})
+
+client.on('ready', () => {
+  console.log('Client is ready!')
+})
+
+client.on('qr', qr => {
+  qrcode.generate(qr, {small: true})
+})
+
+client.on('authenticated', () => {
+  console.log('Authenticated')
+})
+
+client.on('message', message => {
+  console.log(message.body)
+
+  if (message.body === '!test') {
+    message.reply('Maaf sedang tidak ingin diganggu')
+  }
+})
+
+client.initialize()
